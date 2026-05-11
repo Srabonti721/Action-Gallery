70458163c0e28f6ff07f57f5755773b1baf5cf30
Srabonti721 <somayaaktarshabonti721@gmail.com>
2026-05-11 09:05:43 +0600

    gallery data ui interface and table design

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 
+import { Suspense } from 'react'
 import './App.css'
 import Banner from './components/Banner/Banner'
 import Footer from './components/Footer/Footer'
 import Navbar from './components/Navbar/Navbar'
 import Table from './components/Table/Table'
 
+const fetchGallery = async()=>{
+  const res = await fetch('gallery.json')
+  return res.json();
+} 
+
 function App() {
+const galleryPromise = fetchGallery()
+// console.log(galleryPromise);
 
 
   return (
@@ -13,11 +21,17 @@ function App() {
 <Navbar></Navbar>
 <Banner></Banner>
 <main className='min-w-md mx-auto py-10 bg-gray-200  flex '>
-  <div className="left-container w-[70%] pl-10">
-    <h1 className='text-[#0E2954] text-xl'>Action Actives</h1>
-    <p className='text-xm'>Discover and bid on extraordinary items</p>
-    <Table></Table>
+  <div className='left-container w-[70%] pl-10 '>
+        <h1 className='text-[#0E2954] text-xl'>Action Actives</h1>
+      <p className='text-xm'>Discover and bid on extraordinary items</p>
+        <div className=" rounded-box border border-base-content/5 bg-base-100 my-6">
+    <Suspense fallback={<h2>loading....</h2>}>
+      <Table galleryPromise={galleryPromise}></Table>
+    </Suspense>
   </div>
+  </div>
+
+
   <div className="right-container w-[30%]  pr-10">
     <h2>right</h2>
   </div>
---
+++ src/components/Table/SingleTable.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { FcLike } from "react-icons/fc";
+const SingleTable = ({ galleryData }) => {
+  const { image, currentBidPrice, timeLeft, title, description } = galleryData
+  console.log(galleryData);
+  return (
+    <div className=" rounded-box border border-base-content/5">
+      <table className="table">
+        <tbody >
+          {/* row 1 */}
+          <tr>
+            <td>
+              <div className="flex items-center gap-3">
+                <div className="avatar">
+                  <div className="mask h-20 w-20">
+                    <img
+                      src={image}
+                      alt="Avatar Tailwind CSS Component" />
+                  </div>
+                </div>
+                <div>
+                  <h2>{title}</h2>
+                </div>
+              </div>
+            </td>
+            <div className='flex justify-between mr-15'>
+              <td><p>${currentBidPrice}</p></td>
+              <td>{timeLeft}</td>
+              <th>
+                <button className="btn btn-square">
+                  <FcLike size={20} />
+                </button>
+              </th>
+            </div>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default SingleTable;
